b6adc67ba13271ef5f272b3541f49fcdc5433dad
lixin <sadoneli@gmail.com>
2017-06-18 23:19:25 +0800

    push message

--- softcenter/push_message.json.js
+++ softcenter/push_message.json.js
@@ -1,5 +1,5 @@
 {
-"title":"<em>RT-AC86U官改固件&nbsp;-&nbsp;软件中心&nbsp;-&nbsp;bykoolshare</em>",
+"title":"<em>RT-AC86U官改固件&nbsp;-&nbsp;软件中心&nbsp;-&nbsp;by&nbsp;koolshare</em>",
 "content1":"如你所见，我们做了RT-AC86U官改固件的软件中心：",
 "content2":"因为原有所有插件都需要做出一系列调整才能在RT-AC86U上使用，所以目前插件数量还比较少！",
 "content3":"使用本软件中心和软件中心内的软件，必须使用我们发布的改版固件，才能有更好的体验。",
