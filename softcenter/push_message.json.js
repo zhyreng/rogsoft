e94dfe2ab21ae680fdbb9eb69934792a2f06896c
lixin <sadoneli@gmail.com>
2017-06-18 23:23:41 +0800

    push message

--- softcenter/push_message.json.js
+++ softcenter/push_message.json.js
@@ -1,7 +1,7 @@
 {
 "title":"<em>RT-AC86U官改固件&nbsp;-&nbsp;软件中心&nbsp;-&nbsp;by&nbsp;koolshare</em>",
-"content1":"如你所见，我们做了RT-AC86U官改固件的软件中心：",
+"content1":"如你所见，我们做了RT-AC86U官改固件的软件中心，GT-AC5300也将在以后进行适配",
 "content2":"因为原有所有插件都需要做出一系列调整才能在RT-AC86U上使用，所以目前插件数量还比较少！",
 "content3":"使用本软件中心和软件中心内的软件，必须使用我们发布的改版固件，才能有更好的体验。",
-"content4":"如果你有问题，欢迎到我们的论坛：<a href='http://koolshare.cn' target='_blank'><u>http://koolshare.cn</u></a>上面反馈"
+"content4":"如果你有问题，欢迎到我们的论坛：<a href='http://koolshare.cn' target='_blank'><u>http://koolshare.cn</u></a>或者<a href='https://github.com/koolshare/rogsoft' target='_blank'><u>https://github.com/koolshare/rogsoft</u></a>反馈。"
 }
